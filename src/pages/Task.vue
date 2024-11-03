be51dd6f10ab38f0de39be72eb4cc30639899ed3
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-03 10:13:27 +0330

    گرفتن داده ها از بکند و ذخیره در استور و نمایش در کامپوننت

--- src/pages/Task.vue
+++ src/pages/Task.vue
@@ -1,11 +1,25 @@
 <script >
-export default {
+import {useStore} from "vuex";
+import {computed} from "vue";
 
+export default {
+setup(){
+  const store= useStore();
+  const tasks = computed(()=>store.getters.allTasks);
+  function fetchTasks(){
+    store.dispatch("fetchTasks");
+  }
+  fetchTasks();
+  return {
+tasks
+  }
+}
 }
 </script>
 
 <template>
 <h2>Task Component</h2>
+  {{tasks}}
 </template>
 
 <style scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,29 @@
 import {createStore} from "vuex";
+import axios from "axios";
 
 const store = createStore({
     state: {
-        count:2
+        tasks: []
+    },
+    getters: {
+        allTasks: state => state.tasks,
+    },
+    mutations: {
+        setTasks(state, tasks) {
+            state.tasks = tasks;
+        }
+    },
+    actions: {
+        async fetchTasks({commit}) {
+            try{
+                const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+                commit('setTasks', response.data);
+                console.log(response);
+            }catch (error){
+                console.log(error);
+            }
+
+        }
     }
 })
 export default store;
