d1e48a8b484f7ec32993cc8c8ecff756e7cc7440
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-03 09:01:26 +0330

    وارد کردن بوتسترپ در طرح

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
 createApp(App).mount('#app')
