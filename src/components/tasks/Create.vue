e00565fa50409b8130bfb692f7ff92b29d3bd6b1
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-03 13:24:27 +0330

    اضافه کردن دکمه بارگزاری به ایجاد تسک

--- src/components/tasks/Create.vue
+++ src/components/tasks/Create.vue
@@ -8,20 +8,26 @@ export default {
     const store = useStore();
     const title = ref("");
     const titleErrorText= ref("");
+    const loading = ref(false);
+
     async function storeTask(){
       if(title.value === ""){
         titleErrorText.value="title is required";
       }else{
+        loading.value = true;
         titleErrorText.value="";
 
         await store.dispatch('storeTasks',title.value);
-
+        loading.value = false;
         title.value="";
       }
 
     }
     return {
-      storeTask,title,titleErrorText,
+      storeTask,
+      title,
+      titleErrorText,
+      loading,
     }
   }
 }
@@ -39,7 +45,9 @@ export default {
           </div>
         </div>
         <div class="col-auto">
-          <button type="submit" class="btn btn-dark">Create Task</button>
+          <button :disabled="loading" type="submit" class="btn btn-dark">Create Task
+          <span v-if="loading" class="spinner-border spinner-border-sm"></span>
+          </button>
         </div>
 
       </form>
