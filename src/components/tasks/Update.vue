3f244326373d0d676aee7bb74adea921429c5727
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-03 14:37:13 +0330

    تغییر وضعیت تسک به انجام شده و انجام نشده

--- src/components/tasks/Update.vue
+++ src/components/tasks/Update.vue
@@ -1,15 +1,36 @@
-<script >
+<script>
+import {useStore} from "vuex";
+import {ref} from "vue";
+
 export default {
-  props:['task'],
+  props: ['task'],
+  setup() {
+    const store = useStore();
+    const loading = ref(false);
+
+    async function updateTask(task) {
+      loading.value = true;
+      await store.dispatch('updateTask',task);
+      loading.value = false;
+
+    }
+
+
+    return {
+      updateTask,
+      loading,
+    }
+  }
 }
 </script>
 
 <template>
-<div>
-  <span v-if="task.completed" class="bi bi-check-all" style="font-size: 22px"></span>
-  <span v-else class="bi bi-check" style="font-size: 22px"></span>
+  <div>
+    <span v-if="task.completed" @click="updateTask(task)" class="bi bi-check-all" style="font-size: 22px"></span>
+    <span v-else class="bi bi-check" @click="updateTask(task)" style="font-size: 22px"></span>
+    <span v-if="loading" class="spinner-border spinner-border-sm"></span>
 
-</div>
+  </div>
 </template>
 
 <style scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,14 @@ const store = createStore({
         },
         newTask(state, task) {
             state.tasks.unshift(task);
+        },
+        updateTask(state, updateTask) {
+            const index = state.tasks.findIndex(task => task.id === updateTask.id);
+            if (index > -1) {
+                state.tasks.splice(index,1,updateTask);
+            }
         }
+
     },
     actions: {
         async fetchTasks({commit}) {
@@ -61,6 +68,36 @@ const store = createStore({
                 Swal.fire({
                     icon: 'success',
                     title: 'Task created',
+                    showConfirmButton: false,
+                    timerProgressBar: true,
+                    timer: 3000,
+                    toast: true,
+                    position: "top",
+                })
+            }catch (error){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Something went wrong',
+                    confirmButtonText: 'Ok',
+                    cancelButtonText: 'Cancel',
+                })
+            }
+
+        },
+        async updateTask({commit},task) {
+
+            try{
+                const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${task.id}`,{
+                    id: task.id,
+                    title: task.title,
+                    completed: !task.completed,
+                })
+
+                commit('updateTask', response.data);
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Task updated',
                     showConfirmButton: false,
                     timerProgressBar: true,
                     timer: 3000,
