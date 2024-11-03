a9a056dec10dfafc0be0b9b0d745cb65922eb180
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-03 10:47:51 +0330

    قرار دادن لودینگ برای صفحه تسک ها

--- src/pages/Task.vue
+++ src/pages/Task.vue
@@ -1,35 +1,49 @@
-<script >
+<script>
 import {useStore} from "vuex";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 
 export default {
-setup(){
-  const store= useStore();
-  const tasks = computed(()=>store.getters.allTasks);
-  function fetchTasks(){
-    store.dispatch("fetchTasks");
+  setup() {
+    const store = useStore();
+    const tasks = computed(() => store.getters.allTasks);
+    const loading = ref(false);
+
+    async function fetchTasks() {
+      loading.value = true;
+      await store.dispatch("fetchTasks");
+      loading.value = false;
+    }
+
+    fetchTasks();
+    return {
+      tasks,
+      loading
+    }
   }
-  fetchTasks();
-  return {
-tasks
-  }
-}
 }
 </script>
 
 <template>
-<div class="container mt-5">
-  <div class="row g-3">
-    <div v-for="task in tasks" :key="task.id" class="col-md-4 ">
-      <div class="card" :class="{'bg-light':task.completed}">
-        <div class="card-body">
-          <del v-if="task.completed">{{task.title}}</del>
-          <div v-else> {{task.title}}</div>
+  <div>
+    <div v-if="loading" class="container">
+      <div class="row">
+        <div class="spinner-border" role="status"></div>
+      </div>
+    </div>
+    <div v-else class="container mt-5">
+      <div class="row g-3">
+        <div v-for="task in tasks" :key="task.id" class="col-md-4 ">
+          <div class="card" :class="{'bg-light':task.completed}">
+            <div class="card-body">
+              <del v-if="task.completed">{{ task.title }}</del>
+              <div v-else> {{ task.title }}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+
 </template>
 
 <style scoped>
