0b8c92df4b9105de645e4fe0d73192b94bd463e2
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-04 08:45:23 +0330

    نمایش سبد خرید با تعداد موجودی در سبد

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -5,22 +5,81 @@ import {computed} from "vue";
 export default {
   setup() {
     const store = useStore();
-    const products = computed(() => store.getters['product/allProduct']);
-    function addToCart(product){
-      store.dispatch('cart/addToCart',product);
-    }
+    const cartItems = computed(() => store.state.cart.cart);
+
+
     return {
-      products,
-      addToCart
+      cartItems,
     }
   }
 }
 </script>
 
 <template>
-<div>
-  Cart
-</div>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-lg-12 pl-3 pt-3">
+        <table class="table table-hover border bg-white">
+          <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th style="width: 10%">Quantity</th>
+            <th>Subtotal</th>
+            <th>Action</th>
+          </tr>
+          </thead>
+          <tbody>
+
+          <tr v-for="item in cartItems" :key="item.id">
+            <td class="align-middle">
+              <div class="row">
+                <div class="col-lg-2">
+                  <img
+                      :src="item.image"
+                      alt="..."
+                      class="img-fluid"
+                  />
+                </div>
+                <div class="col-lg-10">
+                  <h5>{{ item.name }}</h5>
+                  <p>
+                    {{ item.description }}
+                  </p>
+                </div>
+              </div>
+            </td>
+            <td class="align-middle">{{ item.price }}</td>
+            <td class="align-middle">
+              <button class="btn btn-sm btn-dark me-2">+</button>
+              <span>{{ item.quantity }}</span>
+              <button class="btn btn-sm btn-dark ms-2">-</button>
+            </td>
+            <td class="align-middle">{{ item.price * item.quantity }}</td>
+            <td class="align-middle" style="width: 15%">
+              <button class="btn btn-danger btn-sm">delete</button>
+            </td>
+          </tr>
+
+          </tbody>
+          <tfoot>
+          <tr>
+            <td>
+              <a href="#" class="btn btn-dark">Clear Cart</a>
+            </td>
+            <td colspan="2" class="hidden-xs"></td>
+            <td class="hidden-xs text-center" style="width: 15%">
+              <strong>Total : {{ cartTotalAmount }}</strong>
+            </td>
+            <td>
+              <a href="#" class="btn btn-success btn-block">Checkout</a>
+            </td>
+          </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
