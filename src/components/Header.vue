cf4bd8654a3e0d7c994eb6d344e7c9fed048c615
Rasoulmadani <ali.rahbary@chmail.ir>
2024-11-04 13:09:47 +0330

    تغییر اسم

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,7 +15,7 @@ export default {
 
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-    <a class="navbar-brand ms-4" href="#">WebProg</a>
+    <a class="navbar-brand ms-4" href="#">Rasoul Madani</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
